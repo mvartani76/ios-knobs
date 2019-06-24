832c387995855471e3de89888cbfd1295209e36c
Mike Vartanian <mike.vartanian@gmail.com>
2019-06-24 16:32:49 -0400

    Updated knob to the arc with a marker. Still need to add some constraints to the storyboard.

--- ios-knobs/Knob.swift
+++ ios-knobs/Knob.swift
@@ -12,6 +12,9 @@ func setValue(_ newValue: Float, animated: Bool = false) {
 var isContinuous = true
 
 class Knob: UIControl {
+    
+    private let renderer = KnobRenderer()
+    
     override init(frame: CGRect) {
         super.init(frame: frame)
         commonInit()
@@ -23,9 +26,93 @@ class Knob: UIControl {
     }
     
     private func commonInit() {
-        backgroundColor = .blue
+        renderer.updateBounds(bounds)
+        renderer.color = tintColor
+        renderer.setPointerAngle(renderer.startAngle, animated: false)
+        
+        layer.addSublayer(renderer.trackLayer)
+        layer.addSublayer(renderer.pointerLayer)
     }
 }
 
 private class KnobRenderer {
+    var color: UIColor = .blue {
+        didSet {
+            trackLayer.strokeColor = color.cgColor
+            pointerLayer.strokeColor = color.cgColor
+        }
+    }
+    
+    var lineWidth: CGFloat = 2 {
+        didSet {
+            trackLayer.lineWidth = lineWidth
+            pointerLayer.lineWidth = lineWidth
+            updateTrackLayerPath()
+            updatePointerLayerPath()
+        }
+    }
+    
+    var startAngle: CGFloat = CGFloat(-Double.pi) * 11 / 8 {
+        didSet {
+            updateTrackLayerPath()
+        }
+    }
+    
+    var endAngle: CGFloat = CGFloat(Double.pi) * 3 / 8 {
+        didSet {
+            updateTrackLayerPath()
+        }
+    }
+    
+    var pointerLength: CGFloat = 6 {
+        didSet {
+            updateTrackLayerPath()
+            updatePointerLayerPath()
+        }
+    }
+    
+    private (set) var pointerAngle: CGFloat = CGFloat(-Double.pi) * 11 / 8
+    
+    func setPointerAngle(_ newPointerAngle: CGFloat, animated: Bool = false) {
+        pointerAngle = newPointerAngle
+    }
+    
+    let trackLayer = CAShapeLayer()
+    let pointerLayer = CAShapeLayer()
+    
+    init() {
+        trackLayer.fillColor = UIColor.clear.cgColor
+        pointerLayer.fillColor = UIColor.clear.cgColor
+    }
+    
+    private func updateTrackLayerPath() {
+        let bounds = trackLayer.bounds
+        let center = CGPoint(x: bounds.midX, y: bounds.midY)
+        let offset = max(pointerLength, lineWidth  / 2)
+        let radius = min(bounds.width, bounds.height) / 2 - offset
+        
+        let ring = UIBezierPath(arcCenter: center, radius: radius, startAngle: startAngle,
+                                endAngle: endAngle, clockwise: true)
+        trackLayer.path = ring.cgPath
+    }
+    
+    private func updatePointerLayerPath() {
+        let bounds = trackLayer.bounds
+        
+        let pointer = UIBezierPath()
+        pointer.move(to: CGPoint(x: bounds.width - CGFloat(pointerLength)
+            - CGFloat(lineWidth) / 2, y: bounds.midY))
+        pointer.addLine(to: CGPoint(x: bounds.width, y: bounds.midY))
+        pointerLayer.path = pointer.cgPath
+    }
+    
+    func updateBounds(_ bounds: CGRect) {
+        trackLayer.bounds = bounds
+        trackLayer.position = CGPoint(x: bounds.midX, y: bounds.midY)
+        updateTrackLayerPath()
+        
+        pointerLayer.bounds = trackLayer.bounds
+        pointerLayer.position = trackLayer.position
+        updatePointerLayerPath()
+    }
 }
