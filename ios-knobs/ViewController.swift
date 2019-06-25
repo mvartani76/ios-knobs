1b0a2f13567cbd4211daf434e0891c03ed56eba0
Mike Vartanian <mike.vartanian@gmail.com>
2019-06-25 00:16:39 -0400

    Test code to update values inside knob class.

--- ios-knobs/ViewController.swift
+++ ios-knobs/ViewController.swift
@@ -15,6 +15,9 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        knob.lineWidth = 4
+        knob.pointerLength = 12
     }
 
 
