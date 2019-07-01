8c9cc1154d3cd69e0d5f593dbc31f2191759a52d
Mike Vartanian <mike.vartanian@gmail.com>
2019-07-01 14:41:02 -0400

    Initial Knob3 controller/view/storyboard code. Also changed knob2 stack view name.

---
+++ ios-knobs/Knob3.swift
@@ -0,0 +1,13 @@
+//
+//  Knob3.swift
+//  ios-knobs
+//
+//  Created by Michael Vartanian on 7/1/19.
+//  Copyright © 2019 Michael Vartanian. All rights reserved.
+//
+
+import UIKit.UIGestureRecognizerSubclass
+
+class Knob3: UIControl {
+    
+}
---
+++ ios-knobs/Knob3ViewController.swift
@@ -0,0 +1,33 @@
+//
+//  Knob3ViewController.swift
+//  ios-knobs
+//
+//  Created by Michael Vartanian on 7/1/19.
+//  Copyright © 2019 Michael Vartanian. All rights reserved.
+//
+
+import UIKit
+
+class Knob3ViewController: UIViewController {
+    
+    @IBOutlet var valueLabel: UILabel!
+    @IBOutlet var knob3: Knob3!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
